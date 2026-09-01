b803ff9d4cf6e883aa89ef58c27c25a886884650
mariasn-dev <mariasn.dev@gmail.com>
2026-09-01 12:06:05 +0200

    Remove hello world endpoint

--- index.js
+++ index.js
@@ -39,9 +39,6 @@ let persons = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello world</h1>");
-});
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
